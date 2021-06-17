2374e1e18115084691bb49832ea40c13d997b1a6
NILESH TOSHNIWAL <Nileshtoshniwal743@gmail.com>
2021-06-17 08:58:57 +0530

    new changes

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var obj;
 function search_data()
 {
     var searchDatal=document.getElementById("searchData");
-    cityName.innerText=searchDatal.value;
     console.log(cityName);
 var request = new XMLHttpRequest();
             request.open("GET",`https://api.openweathermap.org/data/2.5/weather?q=${searchDatal.value}&appid=93f26e3c57081a6210de53b8dcfdfea4&units=metric`,true);
@@ -13,6 +12,9 @@ var request = new XMLHttpRequest();
                 {
     if( request.status>=200 && request.status<400)
     {
+                 document.getElementById("searchData").classList.add("is-valid");
+                 document.getElementById("searchData").classList.remove("is-invalid");
+                 cityName.innerText=searchDatal.value;
                 var data = JSON.parse(request.responseText);
                 console.log(data);
                 document.getElementById("temp").innerHTML = Math.round(data.main.temp)+"&deg;C";
@@ -20,11 +22,15 @@ var request = new XMLHttpRequest();
                 document.getElementById("hum").innerHTML = "CLOUDS : "+data.clouds.all+" %";
                 document.getElementById('weather').innerHTML=data.weather[0].description+" | "+"humidity : "+data.main.humidity;
                 imgs_change(data.main.temp,data.weather[0].description);
-                // var icon = data.weather[0].icon+".png";
-                // document.getElementById('myimg').src ="https://openweathermap.org/img/w/"+icon ;
      }
     else 
     {
+        cityName.innerText="Invalid Data";
+        document.getElementById("temp").innerHTML="";
+        document.getElementById("hum").innerHTML ="";
+        document.getElementById('weather').innerHTML="";
+        document.getElementById("searchData").classList.add("is-invalid");
+        document.getElementById("searchData").classList.remove("is-valid");
          console.log("some error");
     }
     };
@@ -60,7 +66,7 @@ function time()
     var month=["JAN","FEB","MAR","APR","MAY","JUNE","JULY","AUG","SEPT","OCT","NOV","DEC"];
     var week=["SUN","MON","TUE","WED","THUS","FRI","SAT"]
     var get=new Date();
-    var pm_am=get.getHours>12?"Pm":"Am";
+    var pm_am=get.getHours()>12?"Pm":"Am";
     var time_str=week[get.getUTCDay()]+' | '+month[get.getMonth()]+" "+ get.getDate()+' | '+get.getHours()%12+":"+get.getMinutes()+":"+get.getSeconds()+" "+pm_am;
     document.getElementById("date").innerText=time_str;
 }
